1c14485c3a089694cdcdab53f32ce44b74031c6d
Manikanta18 <manikanta.jalla8@gmail.com>
2019-06-28 22:02:17 +0530

    changs

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -6,15 +6,18 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import imgUrl from "../media/giphy2.gif";
 import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const styles = {
   card: {
-    width: "40vw",
-    height: "10vw",
-    margin: 50,
-    padding: 25,
+    maxWidth: 800,
+    margin: 40,
+    padding: 20,
     backgroundColor: "#eeeeee",
-    overflow: "auto"
+    overflow: "auto",
+    borderRadius: 25
   },
   root: {
     backgroundImage: "url(" + imgUrl + ")",
@@ -23,15 +26,24 @@ const styles = {
     width: "100vw",
     height: "100vh",
     position: "static"
+  },
+  footer: {
+    height: 120,
+    top: "auto",
+    bottom: 0,
+    width: "100vw",
+    left: 0
+  },
+  footerText: {
+    color: "white"
   }
 };
-
 
 class EventPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      event_array : [
+      event_array: [
         {
           event_name: "Event Name",
           Description: `Event Description Lorem ipsum dolor sit amet, consectetur
@@ -48,26 +60,25 @@ class EventPage extends React.Component {
           numquam dignissimos laborum fugiat deleniti? Eum quasi quidem`,
           event_time: "DD-MM-YYYY Time"
         },
-        {
-          event_name: "Event Name",
-          Description: `Event Description Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Quos blanditiis tenetur unde suscipit, quam
-          beatae rerum inventore consectetur, neque doloribus, cupiditate
-          numquam dignissimos laborum fugiat deleniti? Eum quasi quidem`,
-          event_time: "DD-MM-YYYY Time"
-        },
-        {
-          event_name: "Event Name",
-          Description: `Event Description Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Quos blanditiis tenetur unde suscipit, quam
-          beatae rerum inventore consectetur, neque doloribus, cupiditate
-          numquam dignissimos laborum fugiat deleniti? Eum quasi quidem`,
-          event_time: "DD-MM-YYYY Time"
-        }
-      ],
+        // {
+        //   event_name: "Event Name",
+        //   Description: `Event Description Lorem ipsum dolor sit amet, consectetur
+        //   adipisicing elit. Quos blanditiis tenetur unde suscipit, quam
+        //   beatae rerum inventore consectetur, neque doloribus, cupiditate
+        //   numquam dignissimos laborum fugiat deleniti? Eum quasi quidem`,
+        //   event_time: "DD-MM-YYYY Time"
+        // },
+        // {
+        //   event_name: "Event Name",
+        //   Description: `Event Description Lorem ipsum dolor sit amet, consectetur
+        //   adipisicing elit. Quos blanditiis tenetur unde suscipit, quam
+        //   beatae rerum inventore consectetur, neque doloribus, cupiditate
+        //   numquam dignissimos laborum fugiat deleniti? Eum quasi quidem`,
+        //   event_time: "DD-MM-YYYY Time"
+        // }
+      ]
     };
   }
-
 
   render() {
     const { classes } = this.props;
@@ -75,27 +86,69 @@ class EventPage extends React.Component {
     return (
       <Grid
         className={classes.root}
-        direction="column"
-        justify="center"
-        alignItems="center"
         container
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
       >
-        
         {event_array.map(row => (
           <Card className={classes.card} key={row.id}>
             <CardContent>
               <Typography variant="h6" style={{ color: "#1565c0" }}>
                 {row.event_name}
               </Typography>
-              <Typography variant="body2" style={{ color: "black" }}>
+              <Divider variant="inset" />
+              <Typography
+                variant="body2"
+                style={{ color: "black", marginTop: 20 }}
+              >
                 {row.Description}
               </Typography>
-              <Typography variant="h6" style={{ color: "red" }}>
+              <Typography variant="h6" style={{ color: "red", marginTop: 20 }}>
                 {row.event_time}
               </Typography>
             </CardContent>
           </Card>
         ))}
+
+        <div>
+          <AppBar position="fixed" color="primary" className={classes.footer}>
+            <Toolbar>
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+              >
+                <Typography
+                  variant="h6"
+                  gutterBottom
+                  className={classes.footerText}
+                  style={{ marginTop: 20 }}
+                >
+                  Vishaka Welding Institute
+                </Typography>
+                <Typography
+                  variant="overline"
+                  gutterBottom
+                  className={classes.footerText}
+                  style={{ marginTop: -15 }}
+                >
+                  +91 9704322233, +91 9154165417
+                </Typography>
+                <Typography
+                  variant="overline"
+                  gutterBottom
+                  className={classes.footerText}
+                  style={{ marginTop: -15 }}
+                >
+                  #27-1-245/9, Srinagar, NH_16, Gajuwaka, Vishakapatnam, A.P,
+                  India 530026
+                </Typography>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+        </div>
       </Grid>
     );
   }
